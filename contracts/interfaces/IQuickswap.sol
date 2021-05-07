164e2fac806de8afbcf65900b2aaae6dc542fd43
polyref <83732016+polyref@users.noreply.github.com>
2021-05-07 14:38:03 +0300

    Create IQuickswap.sol

---
+++ contracts/interfaces/IQuickswap.sol
@@ -0,0 +1,10 @@
+interface IQuickSwap {
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+}
