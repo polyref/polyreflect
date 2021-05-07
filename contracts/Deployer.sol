be771bc06bb09b297b2c3a2b26e5dfaa7b975280
polyref <83732016+polyref@users.noreply.github.com>
2021-05-07 14:37:19 +0300

    Update Deployer.sol

--- contracts/Deployer.sol
+++ contracts/Deployer.sol
@@ -5,13 +5,13 @@ contract Deployer is Context, Ownable {
     PolyReflect public reflectToken;
     uint256 internal _tokenDecimals = 9;
     uint256 internal totalRewards = 0;
-    uint256 internal START_TIME = block.timestamp + 60*60*24;
+    uint256 internal START_TIME = block.timestamp + 60 * 60 * 24;
     uint256 internal VALID_TILL = START_TIME + 60 * 60 * 24; // 1 day after start
     uint256 internal immutable TOKENS_FOR_PRESALE = 5_000_000_000 * 10**_tokenDecimals;
     uint256 internal immutable TOKENS_FOR_LIQUIDITY = 4_500_000_000 * 10**_tokenDecimals;
     uint256 internal immutable TEAM_TOKENS = 500_000_000 * 10**_tokenDecimals;
     uint256 internal immutable SOFT_CAP = 250_000 * 10**18; // 250k MATIC
-    uint256 internal immutable PRESALE_RATIO = 8000; // For 1 MATIC you'll receive 8000 PRF
+    uint256 internal immutable PRESALE_RATIO = 8000; // For 1 MATIC you'll receive __PRESALE_RATIO__ PRF
     address internal QUICKSWAP_ROUTER_ADDRESS = 0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff ;
     IQuickSwap internal quick_router;
     
@@ -90,7 +90,7 @@ contract Deployer is Context, Ownable {
     function withdraw() external { // Participans can withdraw their balance in case of unpredictable events 1 hour after presale ends.
         address payable sender = payable(_msgSender());
         require(address(this).balance > 0, "Nothing to withdraw");
-        require(block.timestamp >= VALID_TILL + 25, "Function not available at this moment");
+        require(block.timestamp >= VALID_TILL + 60*60, "Function not available at this moment");
         require(balances[sender] > 0, "Cannot withdraw zero balance");
         sender.transfer(balances[sender]);
         balances[sender] = 0;
@@ -108,5 +108,4 @@ contract Deployer is Context, Ownable {
         balances[sender] = balances[sender].add(msg.value);
         totalRewards = totalRewards.add( _getReward(sender) );
     }
-    
 }
